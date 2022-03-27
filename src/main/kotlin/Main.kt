2f5ebd6981202b66a423b34e8b32e125a691d772
stevenwhy <stevenyildirim@gmail.com>
2022-03-27 16:26:06 -0400

    level order traversal

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,32 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.*
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+
+    println("#1 Binary Tree Level Order Traversal -----------------")
+    val root = TreeNode(12)
+    root.left = TreeNode(7, left = TreeNode(9))
+    root.right = TreeNode(1, left = TreeNode(10), right = TreeNode(5))
+    println("Level order traversal ${bfs(root)}")
+}
+
+fun bfs(root: TreeNode): MutableList<List<Int>> {
+    val result = mutableListOf<List<Int>>()
+
+    val queue: Queue<TreeNode> = LinkedList()
+    queue.add(root)
+
+    while(queue.isNotEmpty()) {
+        val arr = ArrayDeque<Int>()
+        val size = queue.size
+        var i = 0
+        while(i < size) {
+            val node = queue.remove()
+            if(node.left != null) queue.add(node.left)
+            if(node.right != null) queue.add(node.right)
+            arr.add(node.value)
+            i++
+        }
+        result.add(arr.toList())
+    }
+    return result
 }
